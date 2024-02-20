b0cecac0f33ac8ce3392fe79afd28335b7fcbb46
jvaijay-aas <vijay.jyothula@amphenol-sensors.com>
2024-02-20 17:33:55 +0530

    Qual

--- src/main/java/com/avsQual/utility/QualificationUtility.java
+++ src/main/java/com/avsQual/utility/QualificationUtility.java
@@ -23,11 +23,11 @@ public class QualificationUtility extends BaseClass {
 
 	// Read TestData from the Excel sheet
 	//Below Path will be used while executing scripts from Eclipse IDE.
-	public static String TestData_sheetPath = System.getProperty("user.dir") + "/src/test/resources/TestData/" + "AVSQualFlow_TestData.xlsx";
+//	public static String TestData_sheetPath = System.getProperty("user.dir") + "/src/test/resources/TestData/" + "AVSQualFlow_TestData.xlsx";
 	
 	//Below Path will be used whle creating an Jar/exe file where the config file will be 
 	//placed present in the jar.exe path location.
-//	public static String TestData_sheetPath = System.getProperty("user.dir") + "/AVSQualFlow_TestData.xlsx";
+	public static String TestData_sheetPath = System.getProperty("user.dir") + "/AVSQualFlow_TestData.xlsx";
 
 
 	static Workbook book;
--- src/test/java/com/avsQual/testcases/AvsQualFlowTest_AllCases.java
+++ src/test/java/com/avsQual/testcases/AvsQualFlowTest_AllCases.java
@@ -83,6 +83,7 @@ public class AvsQualFlowTest_AllCases extends BaseClass{
 			extent = new ExtentReports(System.getProperty("user.dir") + "/test-output/ER_" + "Qualification_STARTTest" + ".html", true);
 			extent.addSystemInfo("TestSuiteName", "QualificationStartTest");
 			extent.addSystemInfo("AVS Version", prop.getProperty("AVS_Version"));
+			extent.addSystemInfo("FirmWare Version", prop.getProperty("FM_Version"));
 			extent.addSystemInfo("User Name", prop.getProperty("User_Name"));
 			System.out.println("Qualification START Test is in Progress..");
 			
@@ -187,7 +188,7 @@ public class AvsQualFlowTest_AllCases extends BaseClass{
 
 			assetDetailsPage = assetHubPage.click_assetTile2(AssetName);
 			
-			//Qualification Count before study
+			/*			//Qualification Count before study
 			int beforeStudyCnt = assetDetailsPage.QualificationCnt();
 			
 			assetDetailsPage.select_Setup(SetupName);
@@ -409,7 +410,7 @@ public class AvsQualFlowTest_AllCases extends BaseClass{
 			//Here 1 represents qualification reports increments by 1 with every iteration
 			sa.assertEquals(beforeStudyCnt , afterStudyCnt -1, "Fail: Qualification Reports are not generated");		
 			
-			sa.assertAll();   
+			sa.assertAll();   */
 		} 
 
 
